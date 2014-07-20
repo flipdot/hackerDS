0f3c3d56f35ab5c8c52c0788f0885e0ac728b9a4
Daniel Huhn <daniel_huhn@outlook.com>
2014-07-20 20:29:23 +0200

    added reload button to the controller if the server is not up, so we can reload the page after its up again

--- apps/HackerDSCore/clientSrc/controller/js/controllers.js
+++ apps/HackerDSCore/clientSrc/controller/js/controllers.js
@@ -1,21 +1,27 @@
 var controllerControllers = angular.module('controllerControllers', [])
 
-controllerControllers.controller('AppListController', ['$scope', '$http', '$location',
-  function ($scope, $http, $location) {
+controllerControllers.controller('AppListController', ['$scope', '$http', '$location', '$route',
+  function ($scope, $http, $location, $route) {
     $http({
       url: "/apps",
       method: "GET"
     })
-    .error(console.log)
+    .error(function(){
+      $scope.apps = [];
+    })
     .success(function(res){
       $scope.apps = res;
     });
-    
+
     $scope.switchToApp = function (app) {
       $location.path('/apps/'+app.name);
       return false;
     };
-    
+
+    $scope.reload = function(){
+      $route.reload()
+    }
+
     hackerDS.display.send("switchToHome");
   }
 ]);
@@ -26,12 +32,12 @@ controllerControllers.controller('AppControlPanelController',
     var appname = $routeParams.appname;
     var newUrl = "/apps/"+appname+"/controller";
     $scope.frameUrl = $sce.trustAsResourceUrl(newUrl);
-    
+
     $scope.backToAppList = function () {
       $location.path('/');
       return false;
     };
-    
+
     hackerDS.display.send('switchApp', appname);
   }
 ]);
